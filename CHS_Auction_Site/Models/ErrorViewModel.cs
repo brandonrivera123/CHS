ee8763cc01d4825f439f8ef4a5375c151b8b2de1
Brandon Rivera <brandon.rivera2014@gmail.com>
2018-11-19 20:31:07 -0500

    Initial

---
+++ CHS_Auction_Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CHS_Auction_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
